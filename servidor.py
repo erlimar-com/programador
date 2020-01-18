f9a14da7a2ddd3928f121bdd84192f16900eba70
Erlimar Silva Campos <erlimar@gmail.com>
2020-01-18 14:46:14 -0300

    Implementa login e listagem de cursos

--- servidor.py
+++ servidor.py
@@ -1,22 +1,34 @@
-# -*- coding: utf-8 -*-
-
 # Copyright (c) 2020 Erlimar Silva Campos. Todos os direitos reservados.
 # Licenciado sobre a licença MIT. Mais informações da licença em LICENSE.
 
 from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    jwt_required,
+    get_jwt_claims,
+    get_jwt_identity
+)
 from datetime import datetime
 from hashlib import sha256
+from uuid import uuid1
+from os import getenv
 
 # -----------------------------------------------------------
 # Configurações da aplicação Flask
 # -----------------------------------------------------------
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+ENV_DATABASE_FILE = getenv('DATABASE_FILE', 'db.sqlite3')
+ENV_JWT_SECRET_KEY = getenv('JWT_SECRET_KEY', str(uuid1()))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % ENV_DATABASE_FILE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = ENV_JWT_SECRET_KEY
 
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 # -----------------------------------------------------------
 # Modelos do banco de dados
@@ -46,32 +58,47 @@ class Curso(db.Model):
     data_cadastro = db.Column(db.DateTime(), nullable=False,
                               default=datetime.utcnow)
 
+# -----------------------------------------------------------
+# Utilitários JWT
+# -----------------------------------------------------------
+@jwt.user_claims_loader
+def jwt_add_claims(usuario):
+    return {
+        'usuario.id': usuario.id,
+        'usuario.email': usuario.email,
+        'usuario.nome': usuario.nome,
+        'usuario.data_cadastro': usuario.data_cadastro
+    }
+
+@jwt.user_identity_loader
+def jwt_identity_lookup(usuario):
+    return usuario.email
 
 # -----------------------------------------------------------
 # Rotas da API 
 # -----------------------------------------------------------
-@app.route("/api/cadastrar", methods=['POST'])
-def api_route_cadastrar():
+@app.route('/api/cadastrar', methods=['POST'])
+def api_cadastrar():
     if not request.is_json:
-        return jsonify(error=True, msg="JSON não identificado na requisição"), 400
+        return jsonify(error=True, msg='JSON não identificado na requisição'), 400
 
-    nome = request.json.get("nome", None)
-    email = request.json.get("email", None)
-    senha = request.json.get("senha", None)
+    nome = request.json.get('nome', None)
+    email = request.json.get('email', None)
+    senha = request.json.get('senha', None)
 
     if not nome or not email or not senha:
-        return jsonify(error=True, msg="Dados inválidos"), 400
+        return jsonify(error=True, msg='Dados inválidos'), 400
 
     # A senha precisa ter pelo menos 6 caracteres
     senha = senha.strip()
 
     if len(senha) < 6:
-        return jsonify(error=True, msg="A senha precisa ter pelo menos 6 caracteres"), 400
+        return jsonify(error=True, msg='A senha precisa ter pelo menos 6 caracteres'), 400
 
     usuario = Usuario.query.filter_by(email=email).first()
 
     if usuario:
-        return jsonify(error=True, msg="O e-mail %s já está cadastrado!" % email), 400
+        return jsonify(error=True, msg='O e-mail %s já está cadastrado!' % email), 400
 
     senha_hash = sha256(senha.encode()).hexdigest()
     novo_usuario = Usuario(nome=nome, email=email, senha=senha_hash)
@@ -79,14 +106,44 @@ def api_route_cadastrar():
     db.session.add(novo_usuario)
     db.session.commit()
 
-    print("Novo usuário: %s" % novo_usuario)
+    return jsonify(msg='Usuário cadastrado com sucesso!')
 
-    return jsonify(msg="Ok")
+@app.route('/api/token', methods=['POST'])
+def api_token():
+    if not request.is_json:
+        return jsonify(error=True, msg='JSON não identificado na requisição'), 400
+
+    email = request.json.get('email', None)
+    senha = request.json.get('senha', None)
+
+    if not email or not senha:
+        return jsonify(error=True, msg='Dados inválidos'), 400
+
+    senha_hash = sha256(senha.encode()).hexdigest()
+    usuario = Usuario.query.filter_by(email=email, senha=senha_hash).first()
+
+    if not usuario:
+        return jsonify(error=True, msg='E-mail ou senha inválidos!'), 400
+
+    access_token = create_access_token(identity=usuario)
+
+    return jsonify(access_token=access_token), 201
+
+@app.route('/api/cursos', methods=['GET'])
+@jwt_required
+def api_listar_cursos():
+    cursos = []
+
+    for curso in Curso.query.order_by(Curso.nome).all():
+        print("Curso %s" % curso)
+        cursos.append({'id': curso.id, 'nome': curso.nome})
+
+    return jsonify(cursos)
 
 # -----------------------------------------------------------
 # Páginas do site
 # -----------------------------------------------------------
-@app.route("/")
+@app.route('/')
 def page_index():
     return render_template('index.html')
 
